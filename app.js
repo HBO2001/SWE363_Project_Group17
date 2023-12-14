aa54de848509fc8d4e74442d6f9fed1cbabe6fa2
HBO2001 <143583650+HBO2001@users.noreply.github.com>
2023-12-14 15:58:30 +0300

    Add sign up validation

--- app.js
+++ app.js
@@ -1,16 +1,77 @@
 const express = require("express");
+
 const path = require("path");
+
+const bodyParser = require("body-parser");
+
+const {check, validationResult} = require('express-validator')
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+const urlencodedParser = bodyParser.urlencoded({extended:false})
+
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.post('/', urlencodedParser, [
+
+check(('full-name', 'email', 'password', 'confirm-password'), 'Please fill the remaining values')
+  .exists()
+  .notEmpty(),
+
+check('email', 'Email is not valid')
+.isEmail()
+.normalizeEmail(),
+
+check('password', 'Password must be 8 charatcters long at least')
+
+.isLength({min:8}),
+
+
+check('password', 'Password must have 1 capital letter at least')
+.matches(/[A-Z]/),
+
+// check('confirm-password', 'Passwords are not equal')
+// .matches('password')
+
+
+
+
+], (req, res)=>{
+
+
+const errors = validationResult(req)
+
+const PassWord = req.body['password'];
+
+const ConfirmPassword = req.body['confirm-password']
+
+
+if(!errors.isEmpty() || PassWord != ConfirmPassword){
+
+  const alert = errors.array()
+
+  res.render('sign-up',{alert})
+
+}
+
+
+
+
+
+else{
+
+  res.render('home')
+}
+
+})
 
 app.get("/members/profile", (req, res) => {
   res.render("profile");
